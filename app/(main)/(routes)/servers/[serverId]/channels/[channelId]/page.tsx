5a0c77f28886e63dc36778340768949d691c1a0f
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-08-25 02:44:10 +0200

    26: chat input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { db } from "@/lib/db";
 
 interface ChannelIdPageProps {
@@ -44,6 +45,16 @@ const ChannelIdPage = async ({
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
    );
