dd8c15702464aebcfed4daf162dd30c37085e13e
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-08-25 03:08:11 +0200

    27: messages api

---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest } from "next";
+import { getAuth } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+
+export const currentProfilePages = async (req: NextApiRequest) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    return null;
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId
+    }
+  });
+
+  return profile;
+}
