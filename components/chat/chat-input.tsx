838c7e3fc6143415cd6a4ee9fbc93c47514b6c6e
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-08-25 04:00:48 +0200

    29: emoji

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -5,7 +5,8 @@ import axios from "axios";
 import qs from "query-string";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 import {
   Form,
@@ -15,6 +16,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useModal } from "@/hooks/use-modal-store";
+import { EmojiPicker } from "@/components/emoji-picker";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -34,6 +36,7 @@ export const ChatInput = ({
   type,
 }: ChatInputProps) => {
   const { onOpen } = useModal();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -52,6 +55,9 @@ export const ChatInput = ({
       });
 
       await axios.post(url, values);
+
+      form.reset();
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
@@ -81,7 +87,9 @@ export const ChatInput = ({
                     {...field}
                   />
                   <div className="absolute top-7 right-8">
-                    <Smile />
+                    <EmojiPicker
+                      onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                    />
                   </div>
                 </div>
               </FormControl>
---
+++ components/emoji-picker.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Smile } from "lucide-react";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
+import { useTheme } from "next-themes";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface EmojiPickerProps {
+  onChange: (value: string) => void;
+}
+
+export const EmojiPicker = ({
+  onChange,
+}: EmojiPickerProps) => {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Smile
+          className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+        />
+      </PopoverTrigger>
+      <PopoverContent 
+        side="right" 
+        sideOffset={40}
+        className="bg-transparent border-none shadow-none drop-shadow-none mb-16"
+      >
+        <Picker
+          theme={resolvedTheme}
+          data={data}
+          onEmojiSelect={(emoji: any) => onChange(emoji.native)}
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
